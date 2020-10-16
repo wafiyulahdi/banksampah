4f2a8f16f97c5ab8a484edc2ebd25899559a5427
dianafahma <nfahmasalsabila@gmail.com>
2020-10-16 22:38:41 +0700

    Menambahkan class nasabah dan tanggal

--- Saldo/Saldo.java
+++ Saldo/Saldo.java
@@ -1,3 +1,15 @@
+import java.util.Date;
+
+class nasabah{
+	String nama;
+	String idNasabah;
+
+	nasabah(String inputNama, String inputIdNasabah){
+		nama = inputNama; 
+		idNasabah = inputIdNasabah;	
+	}
+}
+
 class checkSaldo{
 	static int saldoAkhir;
 	static int saldoAwal;
@@ -19,13 +31,26 @@ class checkSaldo{
 
 public class Saldo{
 	public static void main (String [] args){
+		int uang;
+
+		//object class check saldo
 		checkSaldo check = new checkSaldo();
 
+		//obj untuk class nasabah
+		nasabah cust1 = new nasabah(" otong", "H96219058");
+
+		//obj untuk tanggal 
+		Date date = new Date();
+
+		//untuk fitur check saldo
 		//read and write dengan menggunakan public
 		check.saldoAwal = 1000; //write
 		check.saldoAkhir = 150;
-		System.out.println("saldo awal : " + check.saldoAwal); //read
-		System.out.println("saldo akhir : " + check.saldoAkhir);
-		System.out.println("saldo totalnya : " + check.getSaldoTotal());
+		System.out.println("Nama 		   :" + cust1.nama);
+		System.out.println("idNasabah	   : " + cust1.idNasabah);
+		System.out.println("Waktu          : " + date.toString());
+		System.out.println("Saldo Awal     : " + check.saldoAwal); //read
+		System.out.println("Saldo akhir    : " + check.saldoAkhir);
+		System.out.println("Saldo Totalnya : " + check.getSaldoTotal());
 	}
 }
