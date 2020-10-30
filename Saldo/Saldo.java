2a8bd0c1d6a3a06df9683543d634913bedccc8c6
dianafahma <nfahmasalsabila@gmail.com>
2020-10-30 22:32:18 +0700

    mengimplementasikan inheritance

--- Saldo/Saldo.java
+++ Saldo/Saldo.java
@@ -1,56 +1,61 @@
-import java.util.Date;
+import java.util.*;
 
-class nasabah{
-	String nama;
-	String idNasabah;
+class saldoUtama {
+	protected String nama;
+	protected int idNasabah;
+	protected int uang = 10000;
+}
 
-	nasabah(String inputNama, String inputIdNasabah){
-		nama = inputNama; 
-		idNasabah = inputIdNasabah;	
+class saldoAwal extends saldoUtama {
+	protected int getAwal() {
+		return uang;
 	}
 }
 
-class checkSaldo{
-	static int saldoAkhir;
-	static int saldoAwal;
-	static int saldoTotal;
-
-	public int getSaldoAwal(){
-		return this.saldoAwal;
-	}
-
-	public int getSaldoAkhir(){
-		return this.saldoAkhir;
-	}
-
-	public int getSaldoTotal(){
-		saldoTotal = this.saldoAwal + this.saldoAkhir;
-		return this.saldoTotal;
+class tambahSaldo extends saldoUtama {
+	protected int getTambahUang(int simpan){
+		uang = uang + simpan;
+		return uang;
 	}
 }
 
-public class Saldo{
-	public static void main (String [] args){
-		int uang;
+public class Saldo {
+	public static void main (String [] args) {
 
-		//object class check saldo
-		checkSaldo check = new checkSaldo();
-
-		//obj untuk class nasabah
-		nasabah cust1 = new nasabah(" otong", "H96219058");
-
-		//obj untuk tanggal 
-		Date date = new Date();
-
-		//untuk fitur check saldo
-		//read and write dengan menggunakan public
-		check.saldoAwal = 1000; //write
-		check.saldoAkhir = 150;
-		System.out.println("Nama 		   :" + cust1.nama);
-		System.out.println("idNasabah	   : " + cust1.idNasabah);
-		System.out.println("Waktu          : " + date.toString());
-		System.out.println("Saldo Awal     : " + check.saldoAwal); //read
-		System.out.println("Saldo akhir    : " + check.saldoAkhir);
-		System.out.println("Saldo Totalnya : " + check.getSaldoTotal());
+		saldoUtama saldo1 = new saldoUtama();
+		saldoAwal saldoAwal1 = new saldoAwal();
+		tambahSaldo tambahSaldo1 = new tambahSaldo();
+		Scanner in = new Scanner(System.in);
+			while (true) {
+				System.out.println("\n\nSelamat Datang di aplikasi Saldo Bank Sampah");
+				System.out.println("---- Menu Aplikasi Saldo Bank Sampah ----");
+				System.out.println("|			1. Saldo Utama 				|");
+				System.out.println("|			2. Tambah Saldo 			|");
+				System.out.println("|			3. Exit 	 				|");
+				/*System.out.println("|			3. Saldo Total 				|");*/
+				/*System.out.println("|			4. Ambil Saldo 				|");*/
+				System.out.println("-----------------------------------------");
+				System.out.print("Masukkan pilihan anda : ");
+				int jawab = in.nextInt();
+				switch (jawab){
+				case 1:
+				//untuk mengambil nilai atau mereturn method uang
+				int saldo = saldoAwal1.getAwal();
+				System.out.println("saldo anda adalah " + saldo);			
+				break;
+				case 2:
+				System.out.print("\nberapa uang yang ingin anda masukkan : ");
+				int x = in.nextInt();
+				//untuk mengambil nilai atau mereturn method simpan uang
+				saldo = tambahSaldo1.getTambahUang(x);
+				System.out.print("anda menyimpan uang sebesar " + saldo);	
+				break;
+				case 3:
+				//untuk kembali ke pilihan yang pertama  
+				System.exit(1);
+				default:
+				System.out.println("no anda tidak valid " ); 	
+			}
+		}
 	}
 }
