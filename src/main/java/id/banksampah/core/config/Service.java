071915e23f9b9064be7b47b5be69a40f675beaef
wafiyulahdi <72172827+wafiyulahdi@users.noreply.github.com>
2020-12-26 07:02:09 +0700

    Update Service.java

--- src/main/java/id/banksampah/core/config/Service.java
+++ src/main/java/id/banksampah/core/config/Service.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package id.banksampah.core.config;
+package id.banksampah.app.core.config;
 
 import id.banksampah.app.core.config.MySQL;
 import java.sql.Connection;
