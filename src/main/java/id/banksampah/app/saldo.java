87679648b85d60bff695233b86b436f8ba817b97
dianafahma <nfahmasalsabila@gmail.com>
2021-01-06 20:52:06 +0700

    add new file saldo

--- src/main/java/id/banksampah/app/saldo.java
+++ src/main/java/id/banksampah/app/saldo.java
@@ -3,11 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package saldo;
+package id.banksampah.app;
 
+import id.banksampah.app.view.ViewSaldo;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+//import id.banksampah.app.view.SaldoViewNew;
+//import id.banksampah.app.view.windowView;
 /**
  *
- * @author Asus
+ * @author AsusS
  */
 public class saldo {
 
@@ -15,7 +20,17 @@ public class saldo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ViewSaldo();
+            }
+        });
     }
-    
 }
