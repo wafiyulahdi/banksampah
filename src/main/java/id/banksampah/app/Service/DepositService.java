186454f321bd6dc2c0b8ce22cac680f5a44d4c43
dianafahma <nfahmasalsabila@gmail.com>
2021-01-06 23:35:30 +0700

    update file in folder core

--- src/main/java/id/banksampah/app/Service/DepositService.java
+++ src/main/java/id/banksampah/app/Service/DepositService.java
@@ -4,11 +4,55 @@
  * and open the template in the editor.
  */
 package id.banksampah.app.service;
+import id.banksampah.app.core.config.Service;
+import id.banksampah.app.core.DepositImp;
+import id.banksampah.app.core.config.ConfigDatabase;
+import id.banksampah.app.model.Deposit;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 /**
  *
  * @author Asus
  */
-public class DepositService {
-    
+public class DepositService extends Service implements DepositImp{
+
+    @Override
+    public boolean checkDeposit(String name) {
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement("SELECT * FROM nasabah WHERE email = ?"); //queery yang akan dieksekusi
+            stmt.setString(1, name); 
+
+            ResultSet result = stmt.executeQuery(); // eksekusi querry
+
+            if (result.next()) { // memindahkan penunjuk objek ke baris berikutnya
+                return true;
+            }
+
+            return false;
+        } catch (SQLException ex) {
+            Logger.getLogger(DepositService.class.getName()).log(Level.SEVERE, null, ex); // menginformsikan aktifitas sistem
+            return false;
+        }
+    }
+
+    @Override
+    public boolean functionInsert(Deposit deposit) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }    
+
+    private class connection {
+
+        public connection() {
+        }
+    }
 }
--- src/main/java/id/banksampah/app/Service/WithdrawService.java
+++ src/main/java/id/banksampah/app/Service/WithdrawService.java
@@ -4,11 +4,32 @@
  * and open the template in the editor.
  */
 package id.banksampah.app.service;
+import id.banksampah.app.core.config.ConfigDatabase;
+import id.banksampah.app.core.DepositImp;
+import id.banksampah.app.core.WithdrawImp;
+import id.banksampah.app.model.Withdraw;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 /**
  *
  * @author Asus
  */
-public class WithdrawService {
+public class WithdrawService extends ConfigDatabase implements WithdrawImp{
+
+    @Override
+    public boolean functionInsert(Withdraw withdraw) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean checkWithdraw(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
