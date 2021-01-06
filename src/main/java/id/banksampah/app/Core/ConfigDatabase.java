70a201d8824d9eab5a8976584b3a196b3031732e
dianafahma <nfahmasalsabila@gmail.com>
2021-01-06 21:51:36 +0700

    update file : delete and add file configdatabase

--- src/main/java/id/banksampah/app/Core/ConfigDatabase.java
+++ src/main/java/id/banksampah/app/Core/ConfigDatabase.java
@@ -5,18 +5,24 @@
  */
 package id.banksampah.app.core;
 
+import java.sql.Connection;
+
 /**
  *
  * @author Asus
  */
-public class ConfigurationDatabase {
+public class ConfigDatabase {
     private String HOSTNAME = "127.0.0.1";
     private String USERNAME = "root";
     private String PASSWORD = "";
-    private String DBNAME = "banksampah";
+    private String DBNAME = "saldobanksampah";
     private int PORT = 3306;
 
     protected String getJdbcDatabaseUrl() {
         return String.format("jdbc:mysql://%s:%s@%s:%d/%s?serverTimezone=UTC", this.USERNAME, this.PASSWORD, this.HOSTNAME, this.PORT, this.DBNAME);
+    }
+
+    public Connection getConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 } 
