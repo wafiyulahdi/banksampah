1f625fa1fb0f0c184fce0e78a6ab0d5b29f5aa1d
dianafahma <nfahmasalsabila@gmail.com>
2021-01-13 22:22:30 +0700

    update configuration

--- src/main/java/id/banksampah/app/Core/config/Service.java
+++ src/main/java/id/banksampah/app/Core/config/Service.java
@@ -5,10 +5,17 @@
  */
 package id.banksampah.app.core.config;
 
+import java.sql.Connection;
+
 /**
  *
  * @author Asus
  */
 public class Service {
-    
+
+    protected Connection connection; // deklarasi koneksi dengan modifier protected agar dapat diakses clas turunan
+
+    public Service() {
+        this.connection = (new MySQL()).getConnection(); // mengambil koneksi agar bisa melakukan querry
+    }
 }
